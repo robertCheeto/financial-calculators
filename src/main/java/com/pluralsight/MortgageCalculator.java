ef74fd854701e412650a066d9c384aef8531f128
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-26 11:14:24 -0400

    * added Scanner variable called keyboard * wrote sout statements to inform the user of the program and to input numerical data for the program * temporarily made interest rate a pre-defined value to test formula * added temp values for principal and loanLength prior to adding values from user * added sout statement to verify inputs were accepted

--- src/main/java/com/pluralsight/MortgageCalculator.java
+++ src/main/java/com/pluralsight/MortgageCalculator.java
@@ -6,12 +6,27 @@ public class MortgageCalculator {
     public static void main(String[] args) {
         // accept the principal, interest rate, and loan length from user
         // display expected monthly payment and total interest paid
-        double principal;
+        double principal = 0;
         double annualInterestRate = 0.07625;
-        double loanLength;
+        double loanLength = 0;
         double numMonthlyPayments = (12 * loanLength);
         double interestRate = (annualInterestRate / 12);
         double monthlyPayment = principal * ((interestRate * Math.pow(1 + interestRate, numMonthlyPayments)) / (Math.pow(1 + interestRate, numMonthlyPayments) - 1));
         double totalInterest = (monthlyPayment * numMonthlyPayments) - principal;
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("*** Welcome to the Mortgage Calculator ***\n");
+        System.out.print("Please enter the principal amount: ");
+        principal = keyboard.nextDouble();
+        System.out.println(principal);
+        // System.out.print("Please enter the interest rate: \n");
+        // annualInterestRate = keyboard.nextDouble();
+        // TO-DO - ALLOW THE USER TO INPUT THEIR OWN INTEREST RATE
+        System.out.print("Please enter the loan length in years: ");
+        loanLength = keyboard.nextDouble();
+        System.out.println(loanLength);
+
+        System.out.println("\nCalculating loan details...\n");
+
     }
 }
