076f47229fe5e70bfd4125e51ac2597a8889855c
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-26 11:46:04 -0400

    * made comments on what the program needs to do to function * refactored file name to correct spelling

---
+++ src/main/java/com/pluralsight/CertificateOfDeposit.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+public class CertificateOfDeposit {
+    public static void main(String[] args) {
+        // accept the deposit amount, interest rate, and years from user
+        // display the future value and total interest earned
+
+
+    }
+}
--- src/main/java/com/pluralsight/CertificateOfDeposite.java
+++
@@ -1,9 +0,0 @@
-package com.pluralsight;
-
-import java.util.Scanner;
-
-public class CertificateOfDeposite {
-    public static void main(String[] args) {
-
-    }
-}
