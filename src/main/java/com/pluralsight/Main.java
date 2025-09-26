3ea9e3b6ae8dfdc8bcbcd162fc3f862ad052c645
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-26 14:06:27 -0400

    * added CertificateOfDeposit method to Main

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -13,9 +13,9 @@ public class Main {
         if (menuChoice == 1) {
             MortgageCalculator();
         }
-//        else if (menuChoice == 2) {
-//            CertificateofDeposit();
-//        }
+        else if (menuChoice == 2) {
+            CertificateofDeposit();
+        }
 //        else if (menuChoice == 3) {
 //            AnnuityCalculator();
 //        }
@@ -61,6 +61,35 @@ public class Main {
         System.out.printf("A loan with a principal balance of $%.2f at an interest rate of %.4f%% for %.0f years ", principal, annualInterestRateDisplay, loanLength);
         System.out.printf("would have a monthly payment of $%.2f with a total interest payment of $%.2f.", monthlyPayment, totalInterest);
 
+    }
+
+    public static void CertificateofDeposit() {
+        // accept the deposit amount, interest rate, and years from user
+        // display the future value and total interest earned
+        double principal;
+        double annualInterestRateDisplay;
+        double years;
+        int daysPerYear = 365;
+        Scanner keyboard = new Scanner (System.in);
+
+
+        System.out.println("*** Welcome to the CD Calculator ***\n");
+        System.out.print("Please enter the principal amount: ");
+        principal = keyboard.nextDouble();
+
+        System.out.print("Please enter the interest rate: ");
+        annualInterestRateDisplay = keyboard.nextDouble();
+        double annualInterestRateCalc = (annualInterestRateDisplay / 100.0);
+
+        System.out.print("Please enter the loan length in years: ");
+        years = keyboard.nextDouble();
+
+        double totalNumberOfDays = daysPerYear * years;
+        double futureValue = principal * (Math.pow(1 + (annualInterestRateCalc / daysPerYear), totalNumberOfDays));
+        double totalInterest = futureValue - principal;
+
+        System.out.printf("\nIf you deposit $%.2f at an interest rate of %.3f%% for %.0f years...", principal, annualInterestRateDisplay, years);
+        System.out.printf("\nYou your final balance would be $%.2f with an interest amount of $%.2f.", futureValue, totalInterest);
     }
 
 }
