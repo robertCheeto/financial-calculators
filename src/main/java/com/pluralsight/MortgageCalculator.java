089c4c55a500f71aac448cba41fcb1ab184e03f7
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-26 11:35:45 -0400

    * still troubleshooting how to properly display 7.625% * removed commented out code * added code to allow user to input their own interest rate * need to work on code to properly calculate annualInterest rate after user inputs value * had idea to create two interest rate values, one to display to the user, one to calculate payment/loan

--- src/main/java/com/pluralsight/MortgageCalculator.java
+++ src/main/java/com/pluralsight/MortgageCalculator.java
@@ -7,18 +7,16 @@ public class MortgageCalculator {
         // accept the principal, interest rate, and loan length from user
         // display expected monthly payment and total interest paid
         double principal;
-        double annualInterestRate = 0.07625;
+        double annualInterestRate;
         double loanLength;
-        //double monthlyPayment = principal * ((interestRate * Math.pow(1 + interestRate, numMonthlyPayments)) / (Math.pow(1 + interestRate, numMonthlyPayments) - 1));
-        //double totalInterest = (monthlyPayment * numMonthlyPayments) - principal;
         Scanner keyboard = new Scanner(System.in);
 
         System.out.println("*** Welcome to the Mortgage Calculator ***\n");
         System.out.print("Please enter the principal amount: ");
         principal = keyboard.nextDouble();
-        // System.out.print("Please enter the interest rate: \n");
-        // annualInterestRate = keyboard.nextDouble();
-        // TO-DO - ALLOW THE USER TO INPUT THEIR OWN INTEREST RATE
+         System.out.print("Please enter the interest rate: \n");
+         annualInterestRate = keyboard.nextDouble();
+        // TO-DO - CREATE VARIABLE FOR INTEREST RATE FOR CALCULATION AND KEEP USER INPUT VARIABLE FOR DISPLAY MESSAGE
         System.out.print("Please enter the loan length in years: ");
         loanLength = keyboard.nextDouble();
 
@@ -29,8 +27,6 @@ public class MortgageCalculator {
         double totalInterest = (monthlyPayment * numMonthlyPayments) - principal;
 
         System.out.printf("A loan with a principal balance of $%.2f at an interest rate of %f%% for %.0f years ", principal, annualInterestRate, loanLength);
-        //System.out.print("A loan with a principal balance of $" + principal + " at an interest rate of " + annualInterestRate +"% for " + loanLength + " years");
-        //System.out.print(" would have a monthly payment of $" + monthlyPayment + " with a total interest of $" + totalInterest);
         System.out.printf("would have a monthly payment of $%.2f with a total interest payment of $%.2f", monthlyPayment, totalInterest);
 
     }
