bb356dec3e2b7712198f5bd72af9864edc9b263a
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-26 12:19:34 -0400

    * added variables and sout statements asking for user inputs * added calculations for finding futureValue * started working on sout statements for CD details messages

--- src/main/java/com/pluralsight/CertificateOfDeposit.java
+++ src/main/java/com/pluralsight/CertificateOfDeposit.java
@@ -4,7 +4,24 @@ public class CertificateOfDeposit {
     public static void main(String[] args) {
         // accept the deposit amount, interest rate, and years from user
         // display the future value and total interest earned
+        double principal;
+        double annualInterestRateDisplay;
+        double years;
+        int daysPerYear = 365;
 
 
+        System.out.println("*** Welcome to the CD Calculator ***\n");
+        System.out.print("Please enter the principal amount: ");
+        principal = keyboard.nextDouble();
+        System.out.print("Please enter the interest rate: ");
+        annualInterestRateDisplay = keyboard.nextDouble();
+        double annualInterestRateCalc = (annualInterestRateDisplay / 100.0);
+        System.out.print("Please enter the loan length in years: ");
+        years = keyboard.nextDouble();
+
+        double totalNumberDays = daysPerYear * years;
+        double futureValue = principal * (1 + Math.pow((annualInterestRateCalc / daysPerYear), daysPerYear * years));
+
+        System.out.println();
     }
 }
