086921fb0b5b1e592faaa8c6efcf016e4422c165
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-26 11:00:40 -0400

    * added comments for what program needs to accomplish * created variables for performing calculation

--- src/main/java/com/pluralsight/MortgageCalculator.java
+++ src/main/java/com/pluralsight/MortgageCalculator.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class MortgageCalculator {
     public static void main(String[] args) {
-
+        // accept the principal, interest rate, and loan length from user
+        // display expected monthly payment and total interest paid
+        double principal;
+        double annualInterestRate = 0.07625;
+        double loanLength;
+        double numMonthlyPayments = (12 * loanLength);
+        double interestRate = (annualInterestRate / 12);
+        double monthlyPayment = principal * ((interestRate * Math.pow(1 + interestRate, numMonthlyPayments)) / (Math.pow(1 + interestRate, numMonthlyPayments) - 1));
+        double totalInterest = (monthlyPayment * numMonthlyPayments) - principal;
     }
 }
